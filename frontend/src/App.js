581a5103145519b418c6e451a021c5c30d812f54
katpvu <vu.kathy98@gmail.com>
2023-05-08 15:50:12 -0700

    add swiper package, create recipe cards and styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import MainPage from './components/MainPage/MainPage';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getCurrentUser } from './store/session';
+import RecipeIndexPage from './components/RecipeIndexPage/RecipeIndexPage';
 
 function App() {
   const [loaded, setLoaded] = useState(false)
@@ -24,6 +25,8 @@ function App() {
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginForm} />
       <AuthRoute exact path="/signup" component={SignupForm} />
+
+      <ProtectedRoute path="/recipes" component={RecipeIndexPage} />
     </Switch>
     </>
   );
---
+++ frontend/src/components/RecipeIndexPage/RecipeIndexPage.js
@@ -0,0 +1,22 @@
+import Header from "../Header/Header";
+import RecipeIndex from "./RecipeIndex";
+import "./RecipeIndex.css"
+
+const RecipeIndexPage = (props) => {
+    // need to fetch recipes based on params
+
+    return (
+        <>
+            <Header />
+            <div className="below-header-container">
+                <div class="side-region-text">
+                    <h1>JAPAN</h1>
+                </div>
+                <h1 className="recipe-index-title">FOLLOW ALONG RECIPES</h1>
+                <RecipeIndex />
+                {/* <RecipeIndex recipes={recipes}/> */}
+            </div>
+        </>
+    )
+};
+export default RecipeIndexPage;
