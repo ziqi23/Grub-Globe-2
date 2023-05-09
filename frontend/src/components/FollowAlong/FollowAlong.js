f7250efb64508f6db12a15ed47a30dd49790f7ea
katpvu <vu.kathy98@gmail.com>
2023-05-09 11:11:59 -0700

    add follow along folder structure

---
+++ frontend/src/components/FollowAlong/FollowAlong.js
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import './FollowAlong.css'
+import { Pagination, Navigation } from "swiper";
+
+const FollowAlong = ({recipe}) => {
+    return (
+        <>
+        <Swiper
+          pagination={{
+            type: "fraction",
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </>
+    )
+};
+
+export default FollowAlong
