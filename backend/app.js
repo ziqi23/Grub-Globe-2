3d7d02a3327d386b896b1e31a4d1a5c498db59e7
Leah Seyoum <leahseyoum@gmail.com>
2023-05-10 10:02:42 -0700

    added space

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(passport.initialize());
+app.use(passport.initialize()) ;
 
 if (!isProduction) {
   const path = require('path');
