fb82113dad5abef2b87ffedbec3989fa301b0dd6
Ziqi Zou <brianzou1@gmail.com>
2023-05-11 13:48:06 -0700

    update users model

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,20 +2,28 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-      type: String,
-      required: true
-    },
-    hashedPassword: {
-      type: String,
-      required: true
-    }
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  profilePhoto: {
+    type: Buffer 
+  }
 }, {
     timestamps: true
 })
 
 module.exports = mongoose.model('User', userSchema);
+
