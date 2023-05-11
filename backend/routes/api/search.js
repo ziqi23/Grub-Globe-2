f2fe2a2c44bd053d039d5aa95cb4bb295a0642cc
Leah Seyoum <leahseyoum@gmail.com>
2023-05-11 11:26:35 -0700

    updated search with select

--- backend/routes/api/search.js
+++ backend/routes/api/search.js
@@ -5,21 +5,45 @@ const Recipe = mongoose.model('Recipe');
 
 router.get('/', async (req, res) => {
   const query = req.query.q;
-  try {
-    const recipes = await Recipe.find({
-      $or: [
-        { recipeName: { $regex: query, $options: "i" } },
-        { country: { $regex: query, $options: "i" } },
-        { tags: { $elemMatch: { $regex: query.replace(/\s+/g, ''), $options: "i" } } },
+  const queryArray = query.split(" , ");
+  console.log(queryArray, 'queryArray');
+  if (queryArray.length === 1) {
+      try {
+        const recipes = await Recipe.find({
+          $or: [
+            { recipeName: { $regex: query, $options: "i" } },
+            { country: { $regex: query, $options: "i" } },
+            { tags: { $elemMatch: { $regex: query.replace(/[-\s]/g, ""), $options: "i" } } },
+            { ingredients: { $elemMatch: { name: { $regex: query, $options: "i" } } } }
+            
+          ]
+    
+        }).sort({ createdAt: -1 });
+        console.log(recipes);
+        return res.json(recipes);
+      } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Internal server error" });
+      }
+  } else if (queryArray.length > 1) {
+    try {
+        const recipes = await Recipe.find({
+          $or: queryArray.map(term => ({
+            $or: [
+              { recipeName: { $regex: term, $options: "i" } },
+              { country: { $regex: term, $options: "i" } },
+              { tags: { $elemMatch: { $regex: term.replace(/[-\s]/g, ""), $options: "i" } } },
+              { ingredients: { $elemMatch: { name: { $regex: query, $options: "i" } } } }
+            ]
+          }))
+        }).sort({ createdAt: -1 });
         
-      ]
-
-    }).sort({ createdAt: -1 });
-    console.log(recipes);
-    return res.json(recipes);
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: "Internal server error" });
+        console.log(recipes);
+        return res.json(recipes);
+      } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Internal server error" });
+      }
   }
 });
 
--- frontend/src/components/SearchBar/Search.js
+++ frontend/src/components/SearchBar/Search.js
@@ -1,15 +1,49 @@
 import { useState, useEffect } from "react";
 import RecipeCard from "../RecipeIndexPage/RecipeCard";
+import CreatableSelect from "react-select/creatable";
+import React from "react";
 
 function RecipeSearch() {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
   const [error, setError] = useState("");
+  const [options, setOptions] = useState([]);
+
+    const countryTagOptions = [  {    label: 'Countries',    
+    options: [      
+    { value: 'United States', label: 'United States' },      
+    { value: 'Mexico', label: 'Mexico' },      
+    { value: 'Italy', label: 'Italy' },      
+    { value: 'France', label: 'France' },      
+    { value: 'Spain', label: 'Spain' },      
+    { value: 'Greece', label: 'Greece' },
+    { value: 'China', label: 'China' },
+    { value: 'Japan', label: 'Japan' },
+    { value: 'India', label: 'India' },
+    { value: 'Thailand', label: 'Thailand' },
+    { value: 'Russia', label: 'Russia' },
+    { value: 'Brazil', label: 'Brazil' },
+    { value: 'Vietnam', label: 'Vietnam' },
+    { value: 'Germany', label: 'Germany' },
+    { value: 'United Kingdom', label: 'United Kingdom' },    ],
+    },
+    {
+        label: 'Tags',
+        options: [
+        { value: 'vegetarian', label: 'vegetarian' },
+        { value: 'vegan', label: 'vegan' },
+        { value: 'gluten-free', label: 'gluten-free' },
+        { value: 'dairy-free', label: 'dairy-free' },
+        { value: 'sustainable', label: 'sustainable' },
+        ],
+    },
+    ];
 
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      console.log(query);
       const response = await fetch(`/api/search?q=${query}`);
       const data = await response.json();
       setResults(data);
@@ -26,21 +60,30 @@ function RecipeSearch() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input
+        {/* <input
           type="text"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-        />
-        <button type="submit">Search</button>
+        /> */}
+            <CreatableSelect
+                isMulti
+                isCreatable={true}
+                options={countryTagOptions}
+                placeholder="Search by country or tag..."
+                onChange={(selected) => {
+                    setQuery(selected.map((option) => option.value).join(" , "));
+                }}
+            />
+        {/* <button type="submit">Search</button> */}
       </form>
       {results.length > 0 ? (
       <ul>
-        {results.map((recipe) => (
-          <li key={recipe.id}>
+        {results.map((recipe, idx) => (
+          <li key={idx}>
             <h3>{recipe.recipeName}</h3>
             {/* <p>{result.description}</p> */}
+             {/* <RecipeCard key={recipe.id} recipe={recipe} /> */}
           </li>
-        // <RecipeCard key={recipe.id} recipe={recipe} />
         ))}
       </ul>
       ) : (
