72a504542f746482b0a09c4333d09592e0451ae0
Ziqi Zou <brianzou1@gmail.com>
2023-05-14 13:10:36 -0700

    finish data visualization and fix globe not rendering dots

--- frontend/src/components/Globe/Globe.js
+++ frontend/src/components/Globe/Globe.js
@@ -270,8 +270,11 @@ function Globe(props) {
       renderer.render(scene, camera);
     }
     animate();
-    console.log(window.innerWidth);
-    // return () => ref.current.removeChild(renderer.domElement);
+    return () => {
+      while (scene.children.length > 0) {
+        scene.remove(scene.children[0])
+      }
+    }
   }, []);
 
   // Use UV coordinates to determine if user is hovering over a in-scope country
@@ -300,7 +303,11 @@ function Globe(props) {
 
   let imgColor;
   useEffect(() => {
-    img.onload = () => createDots();
+    if (img.complete) {
+      createDots()
+    } else {
+      img.onload = () => createDots();
+    }
 
     function createDots() {
       const canvas = document.getElementById("placeholder-canvas");
--- frontend/src/components/RecipeShow/RecipeShowPage.js
+++ frontend/src/components/RecipeShow/RecipeShowPage.js
@@ -23,6 +23,7 @@ import plateIcon from "../../assets/icons/general-icons/icons8-plate-100.png";
 const RecipeShowPage = () => {
   const dispatch = useDispatch();
   const { recipeId } = useParams();
+  const [tooltipOpen, setTooltipOpen] = useState(-1)
 
   const icons = {
     glutenFree: glutenFreeIcon,
@@ -68,6 +69,11 @@ const RecipeShowPage = () => {
   // const mapTags = (tags) => {
   //   return tags.map((tag, i, tags) => {i === (tags.length - 1) ? `${tag.name}` :`${tag.name}, `})
   // }
+  
+  const handleMouseLeave = (e) => {
+    e.preventDefault()
+    setTooltipOpen(-1)
+  }
 
   return (
     <>
@@ -146,9 +152,9 @@ const RecipeShowPage = () => {
           </div>
 
           <div className="macros-container">
-            <div>
+            <div onMouseLeave={handleMouseLeave}>
               <h2>Macronutrients</h2>
-              <Macronutrients macronutrients={recipe?.nutrition.nutrients} />
+              <Macronutrients macronutrients={recipe?.nutrition.nutrients} tooltipOpen={tooltipOpen} setTooltipOpen={setTooltipOpen}/>
             </div>
           </div>
         </div>
