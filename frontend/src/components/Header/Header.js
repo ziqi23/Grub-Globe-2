a779b23db4fe22a100a85cf4394bf0f2611ef0d3
Leah Seyoum <leahseyoum@gmail.com>
2023-05-14 19:05:02 -0700

    added search bar to show page

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -5,12 +5,13 @@ import { useLocation } from 'react-router-dom';
 
 const Header = ({openLoginModal, openSignupModal}) => {
     const location = useLocation();
-    const isRecipePage = location.pathname === '/recipes';
+    const show = location.pathname !== '/explore' && location.pathname !== '/';
+    console.log(show)
 
     return(
         <div className="header-container">
             <div className="logo"><h1>grubGlobe</h1></div>
-            {isRecipePage && <RecipeSearch />}
+            {show && <RecipeSearch />}
             <NavBar openLoginModal={openLoginModal} openSignupModal={openSignupModal}/>
         </div>
     )
