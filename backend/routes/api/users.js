076ffec6981a3ec36f7a380af058f7857c160015
Ziqi Zou <brianzou1@gmail.com>
2023-05-25 08:47:53 -0700

    merge with main

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -45,12 +45,6 @@ router.post(
   }
 );
 
-router.get("/", function (req, res, next) {
-  res.json({
-    message: "GET /users",
-  });
-});
-
 router.get("/current", restoreUser, function (req, res, next) {
   if (!isProduction) {
     const csrfToken = req.csrfToken();
--- backend/validations/register.js
+++ backend/validations/register.js
@@ -16,12 +16,6 @@ const validateRegisterInput = [
     .exists({ checkFalsy: true })
     .isLength({ min: 2, max: 30 })
     .withMessage('Username must be between 2 and 30 characters'),
-  check('firstName')
-    .exists({ checkFalsy: true })
-    .withMessage('First name is required'),
-  check('lastName')
-    .exists({ checkFalsy: true })
-    .withMessage('Last name is required'),
   check('password')
     .exists({ checkFalsy: true })
     .isLength({ min: 6, max: 30 })
