b65285731bf1a6003b9d734fae43a8ef9d9e6a98
katpvu <vu.kathy98@gmail.com>
2023-05-25 17:11:06 -0700

    add error page

---
+++ frontend/src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,17 @@
+import "./ErrorPage.css"
+
+const ErrorPage = () => {
+
+    return (
+        <>
+        <div id="error-page-root">
+            <div id="error-messages">
+                <h1 id="message-404">404</h1>
+                <h1 id="page-not-found">PAGE NOT FOUND</h1>
+            </div>
+        </div>
+        </>
+    )
+};
+
+export default ErrorPage;
