79011ca09e501b226986da644cd121a6ac47bc26
katpvu <vu.kathy98@gmail.com>
2023-05-25 17:17:24 -0700

    add return home button on error page

--- frontend/src/components/ErrorPage/ErrorPage.js
+++ frontend/src/components/ErrorPage/ErrorPage.js
@@ -1,5 +1,5 @@
 import "./ErrorPage.css"
-
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const ErrorPage = () => {
 
     return (
@@ -8,6 +8,8 @@ const ErrorPage = () => {
             <div id="error-messages">
                 <h1 id="message-404">404</h1>
                 <h1 id="page-not-found">PAGE NOT FOUND</h1>
+                <br></br>
+                <Link to="/" id="return-home-button">return home</Link>
             </div>
         </div>
         </>
